e4e8c2b0889af71a6ba9c3dcc4fd56dbbdd6f76d
nathan-08 <nathanryan001@gmail.com>
2017-11-18 17:58:09 -0800

    trouble communicating from frontend build

---
+++ server/middlewares/filter.js
@@ -0,0 +1,10 @@
+notAllowed = ['stupid, asshat, unfriend'];
+
+module.exports =
+    (req, res, next) => {
+        while (notAllowed.find(word => req.body.text.includes(word))) {
+            const badWord = notAllowed.find(word => req.body.text.includes(word));
+            req.body.text = req.body.text.replace(badWord, '*'.repeat(badWord.length));
+        }
+        next()
+    }
---
+++ server/middlewares/session.js
@@ -0,0 +1,9 @@
+module.exports=
+    (req, res, next)=>{
+        if(!req.session.user){
+            req.session.user={
+                messages: []
+            }
+        } 
+        next()
+    }
